49d9c7f8908f82f08fc3cba6386ef5116a13ef95
DavidSolanas <davidsolanas086@gmail.com>
2023-04-23 13:37:23 +0200

    update test script

---
+++ test_command.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python test.py --dataroot /Disco2021-I/david/tfm/dataset/cyclegan_dataset/ --name Train500Volumes --model cycle_gan3d --input_nc 1 --output_nc 1 --gpu_ids 1 --eval --load_iter 50
